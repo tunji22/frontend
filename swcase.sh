385b2a6227ab4c5064f2ec3d6b62ec1e31f04797
tunji22 <olatunjisofela@yahoo.com>
2022-05-17 23:05:16 -0500

    1st deployment

---
+++ swcase.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "learning switch cases in scripting"
+case $1 in
+start)
+echo "starting sonarQube server"
+sleep 5
+echo "sonarQube server started";;
+stop)
+echo "stopping sonarQube server"
+sleep 5
+echo "sonarQube server stopped";;
+restart)
+echo "restarting sonarQube server"
+sleep 5
+echo "sonarQube server restarted";;
+update)
+echo "updating sonarQube server"
+sleep 5
+echo "sonarQube server updated";;
+*)
+echo "sorry you passed the wrong option"
+echo "options are start | stop | restart | update ";;
+esac
+echo "switch cases are very good and easy to understand"
